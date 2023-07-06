d5dafeea34f8cf7ff2613788ede935d84f99bb65
谢先生 <8008121104@email.ncu.edu.cn>
2023-07-06 13:26:54 +0800

    Site updated: 2023-07-06 13:26:54

--- js/no_select.js
+++ js/no_select.js
@@ -27,3 +27,24 @@ document.oncontextmenu = function (event){
 		return false;
 	}
 }
+
+//禁用F12
+document.onkeydown = function(){
+    if(window.event && window.event.keyCode == 123) {
+        alert("唉~~,天天F12,被抓到了吧~~");
+        event.keyCode=0;
+        event.returnValue=false;
+    }
+    if(window.event && window.event.keyCode == 13) {
+        window.event.keyCode = 505;
+    }
+    if(window.event && window.event.keyCode == 8) {
+        alert(str+"\n");
+        window.event.returnValue=false;
+    }
+}
+
+console.log("%c%c博客名称%c Ncuxie's Blog","line-height:28px;","line-height:28px;padding:4px;background:#222;color:#fff;font-size:16px;margin-right:15px","color:#3fa9f5;line-height:28px;font-size:16px;");
+console.log("%c%c网站地址%c ncuxie.github.io","line-height:28px;","line-height:28px;padding:4px;background:#222;color:#fff;font-size:16px;margin-right:15px","color:#ff9900;line-height:28px;font-size:16px;");
+console.log("%c%c企鹅号码%c 想什么呢？怎么可能告诉你啊喂！","line-height:28px;","line-height:28px;padding:4px;background:#222;color:#fff;font-size:16px;margin-right:15px","color:#008000;line-height:28px;font-size:16px;");
+console.log("%c%cThere should be something here.","line-height:28px;","line-height:28px;padding:4px 0px;color:#fff;font-size:16px;background-image:-webkit-gradient(linear,left top,right top,color-stop(0,#ff22ff),color-stop(1,#5500ff));-webkit-background-clip:text;");
