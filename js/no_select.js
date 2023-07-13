1c62999b0165636ebb877a4ae519786821120462
谢先生 <8008121104@email.ncu.edu.cn>
2023-07-14 01:35:44 +0800

    Site updated: 2023-07-14 01:35:42

--- js/no_select.js
+++ js/no_select.js
@@ -44,7 +44,7 @@ document.onkeydown = function(){
     }
 }
 
-console.log("%c%c博客名称%c Ncuxie's Blog","line-height:28px;","line-height:28px;padding:4px;background:#222;color:#fff;font-size:16px;margin-right:15px","color:#3fa9f5;line-height:28px;font-size:16px;");
-console.log("%c%c网站地址%c ncuxie.github.io","line-height:28px;","line-height:28px;padding:4px;background:#222;color:#fff;font-size:16px;margin-right:15px","color:#ff9900;line-height:28px;font-size:16px;");
-console.log("%c%c企鹅号码%c 想什么呢？怎么可能告诉你啊喂！","line-height:28px;","line-height:28px;padding:4px;background:#222;color:#fff;font-size:16px;margin-right:15px","color:#008000;line-height:28px;font-size:16px;");
-console.log("%c%cThere should be something here.","line-height:28px;","line-height:28px;padding:4px 0px;color:#fff;font-size:16px;background-image:-webkit-gradient(linear,left top,right top,color-stop(0,#ff22ff),color-stop(1,#5500ff));-webkit-background-clip:text;");
+console.log("%c%c 博客名称 %c Ncuxie's Blog","line-height:28px;","line-height:28px;padding:4px;background:#222;color:#fff;font-size:16px;margin-right:15px","color:#3fa9f5;line-height:28px;font-size:16px;");
+console.log("%c%c 网站地址 %c ncuxie.github.io","line-height:28px;","line-height:28px;padding:4px;background:#222;color:#fff;font-size:16px;margin-right:15px","color:#ff9900;line-height:28px;font-size:16px;");
+console.log("%c%c 企鹅号码 %c 想什么呢？怎么可能告诉你啊喂！","line-height:28px;","line-height:28px;padding:4px;background:#222;color:#fff;font-size:16px;margin-right:15px","color:#008000;line-height:28px;font-size:16px;");
+console.log("%c%c Something should here.","line-height:28px;","line-height:28px;padding:4px 0px;color:#fff;font-size:16px;background-image:-webkit-gradient(linear,left top,right top,color-stop(0,#ff22ff),color-stop(1,#5500ff));-webkit-background-clip:text;");
