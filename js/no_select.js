bef39d579e0b638ece307988c96565d152ce550b
谢先生 <8008121104@email.ncu.edu.cn>
2023-07-29 00:09:09 +0800

    Site updated: 2023-07-29 00:09:09

--- js/no_select.js
+++ js/no_select.js
@@ -1,4 +1,4 @@
-//禁用选中
+// 禁用选中
 document.onselectstart = function (event){
 	if(window.event){
 		event = window.event;
@@ -13,7 +13,7 @@ document.onselectstart = function (event){
 	}
 }
 
-//禁用右键
+// 禁用右键
 document.oncontextmenu = function (event){
 	if(window.event){
 		event = window.event;
@@ -28,7 +28,7 @@ document.oncontextmenu = function (event){
 	}
 }
 
-//禁用F12
+// 禁用F12
 document.onkeydown = function(){
     if(window.event && window.event.keyCode == 123) {
         alert("唉~~,天天F12,被抓到了吧~~");
@@ -44,7 +44,22 @@ document.onkeydown = function(){
     }
 }
 
+// 禁用图片查看
+var imgs = document.getElementsByTagName("img");
+for (let i = imgs.length - 1; i >= 0; i--) {
+	imgs[i].setAttribute('onclick', 'return false;');
+}
+
+// 控制台内容
 console.log("%c%c 博客名称 %c Ncuxie's Blog","line-height:28px;","line-height:28px;padding:4px;background:#222;color:#fff;font-size:16px;margin-right:15px","color:#3fa9f5;line-height:28px;font-size:16px;");
 console.log("%c%c 网站地址 %c ncuxie.github.io","line-height:28px;","line-height:28px;padding:4px;background:#222;color:#fff;font-size:16px;margin-right:15px","color:#ff9900;line-height:28px;font-size:16px;");
-console.log("%c%c 企鹅号码 %c 想什么呢？怎么可能告诉你啊喂！","line-height:28px;","line-height:28px;padding:4px;background:#222;color:#fff;font-size:16px;margin-right:15px","color:#008000;line-height:28px;font-size:16px;");
-console.log("%c%c Something should here.","line-height:28px;","line-height:28px;padding:4px 0px;color:#fff;font-size:16px;background-image:-webkit-gradient(linear,left top,right top,color-stop(0,#ff22ff),color-stop(1,#5500ff));-webkit-background-clip:text;");
+console.log("%c%c\
+  The fact is that I did not know how to understand anything!\n\
+  I ought to have judged by deeds and not by words.\n\
+  She cast her fragrance and her radiance over me.\n\
+  I ought to never to have run away from her...\n\
+  I ought to have guessed all the affection\n\
+			that lay behind her poor little stratagems.\n\
+  Flowers are so inconsistent!\n\
+  But I was too young to know how to love her...\
+","line-height:28px;","line-height:28px;padding:4px 0px;color:#fff;font-size:16px;");
